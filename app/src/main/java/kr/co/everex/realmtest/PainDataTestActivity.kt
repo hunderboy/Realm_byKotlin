1330cde9eda5525feaf5c22e395adeb12bea7902
Dev_dominos <masury2011@gmail.com>
2021-05-06 04:48:01 +0900

    // 날짜 순차 추출 기능 구성중

--- app/src/main/java/kr/co/everex/realmtest/PainDataTestActivity.kt
+++ app/src/main/java/kr/co/everex/realmtest/PainDataTestActivity.kt
@@ -13,6 +13,7 @@ import kr.co.everex.realmtest.realmobject.RealmPainInfo
 
 class PainDataTestActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPainDataTestBinding
+    private val TAG = "PainDataTestActivity : "
 
     // 데이터 초기화
     var realm: Realm? = null
@@ -35,7 +36,6 @@ class PainDataTestActivity : AppCompatActivity() {
     }
 
     fun deleteData() {
-
         try {
             val id: Long = edt_id.text.toString().toLong()
             val dataModel =
@@ -52,7 +52,6 @@ class PainDataTestActivity : AppCompatActivity() {
         }
     }
     fun updateData() {
-
         try {
 
             val id: Long = edt_id.text.toString().toLong()
@@ -74,36 +73,43 @@ class PainDataTestActivity : AppCompatActivity() {
     }
     fun addData() {
         try {
-            // 데이터 모델
-            dataModel.id = edt_id.text.toString().toInt()
-            dataModel.name = edt_name.text.toString()
-            dataModel.email = edt_email.text.toString()
+            dataModel.userID = binding.edtId.text.toString()
+            dataModel.assignedProgram = binding.edtProgram.text.toString()
+            dataModel.painInfoBeforeExercise = binding.edtBeforePain.text.toString().toInt()
+            dataModel.painInfoAfterExercise = binding.edtAfterPain.text.toString().toInt()
+
+            dataModel.year = binding.edtYear.text.toString().toInt()
+            dataModel.month = binding.edtMonth.text.toString().toInt()
+            dataModel.day = binding.edtDay.text.toString().toInt()
+
+            dataModel.exerciseDay = binding.edtYear.text.toString()+"-"+binding.edtMonth.text.toString()+"-"+binding.edtDay.text.toString()
 
             realm!!.executeTransaction {
                     realm -> realm.copyToRealm(dataModel)
             }
-
             clearFields()
-
-            Log.e("Status","Data Inserted !!!")
-
+            Log.e(TAG,"통증 데이터 추가됨 !!!")
         }catch (e:Exception){
-            Log.e("뭐가 문제야?",e.toString())
-            Log.e("Status","Something went Wrong !!!")
+            Log.e("데이터 추가 뭐가 문제야?",e.toString())
         }
-
     }
     fun readData() {
 
         try {
-            val dataModels: List<DataModel> = realm!!.where(DataModel::class.java).findAll()
-            Log.e("indices",dataModels.indices.toString())
+            val dataModels: List<RealmPainInfo> = realm!!.where(RealmPainInfo::class.java).findAll()
+            Log.e("indices",dataModels.indices.toString()) // 마지막 인덱스 번호?
             for (i in dataModels.indices) {
-                edt_id?.setText(dataModels[i].id.toString())
-                edt_name?.setText(dataModels[i].name)
-                edt_email?.setText(dataModels[i].email)
+
                 Log.e("ReadData , index[$i]",
-                    dataModels[i].id.toString() +" , "+ dataModels[i].name +" , "+ dataModels[i].email)
+                    dataModels[i].exerciseDay +" , "+
+                            dataModels[i].year.toString() +" , "+
+                            dataModels[i].month.toString() +" , "+
+                            dataModels[i].day.toString() +" , "+
+                            dataModels[i].userID +" , "+
+                            dataModels[i].assignedProgram +" , "+
+                            dataModels[i].painInfoBeforeExercise +" , "+    // 운동 전
+                            dataModels[i].painInfoAfterExercise             // 운동 후
+                )
             }
 
             Log.e("Status","Data Fetched !!!")
@@ -131,13 +137,10 @@ class PainDataTestActivity : AppCompatActivity() {
         }
     }
 
-
     // 텍스트 클리어
     fun clearFields(){
         binding.edtBeforePain.setText("")
         binding.edtAfterPain.setText("")
-        binding.edtYear.setText("")
-        binding.edtMonth.setText("")
         binding.edtDay.setText("")
     }
 }
--- app/src/main/java/kr/co/everex/realmtest/realmobject/RealmPainInfo.kt
+++ app/src/main/java/kr/co/everex/realmtest/realmobject/RealmPainInfo.kt
@@ -10,11 +10,18 @@ open class RealmPainInfo  : RealmObject() {
 //    var assignedWeek:String = "없음"         // 할당된 주차
 //    var assignedDay:String = "없음"          // 할당된 일자
 
+//    val datatype : String = "RealmPainInfo"
+
+    @PrimaryKey
+    var exerciseDay : String = "2021-04-21"
+
+    val date : Date = Date()
+
     var year :Int = 2021  // 년
     var month :Int = 4    // 월
     var day :Int = 21     // 일
     var userID :String = "없음"              // 운동하는 사람 아이디
     var assignedProgram:String = "없음"      // 할당된 프로그램
-    var painInfoBeforeExercise:String = ""  // 운동 전 통증정보 (0~10)
-    var painInfoAfterExercise:String = ""   // 운동 후 통증정보 (0~10)
+    var painInfoBeforeExercise:Int = 0  // 운동 전 통증정보 (0~10)
+    var painInfoAfterExercise:Int = 0   // 운동 후 통증정보 (0~10)
 }
